c00054912b8bea9dbc1f741cee69e5e88a4cc90b
Maksim <maxus.info.mail@gmail.com>
2021-08-08 12:44:17 +0300

    Fix
    Signed-off-by: Maksim <maxus.info.mail@gmail.com>

---
+++ src/main/kotlin/space/maxus/refsb/api/SkyblockPlugin.kt
@@ -0,0 +1,29 @@
+package space.maxus.refsb.api
+
+import org.bukkit.plugin.java.JavaPlugin
+import space.maxus.refsb.api.commands.ChatCommand
+import space.maxus.refsb.api.commands.CommandRegisterer
+
+/**
+ * Main class used for inheritance with skyblock related plugins
+ */
+abstract class SkyblockPlugin : JavaPlugin() {
+    var commands : MutableList<ChatCommand> = mutableListOf()
+
+    final override fun onEnable() {
+        CommandRegisterer(this)
+        enable()
+    }
+
+    final override fun onDisable() {
+        disable()
+    }
+
+    open fun enable() {
+
+    }
+
+    open fun disable() {
+
+    }
+}
